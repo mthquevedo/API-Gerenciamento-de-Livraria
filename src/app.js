d63dd56dd9227eb0a115dfcfc5b75de50217f073
mthquevedo <matheusq10@gmail.com>
2025-09-04 21:05:47 -0300

    Adjust: lógicas implementadas em middlewares

--- src/app.js
+++ src/app.js
@@ -1,14 +1,15 @@
 import express from "express";
 import db from "./config/dbConnect.js";
+import manipuladorDeErros from "./middlewares/manipuladorDeErros.js";
+import verficadorDeconexao from "./middlewares/verficadorDeConexao.js";
 import routes from "./routes/index.js";
 
-db.on("error", console.log.bind(console, "Erro de conexão"));
-db.once("open", () => {
-  console.log("conexão com o banco feita com sucesso");
-});
+verficadorDeconexao(db);
 
 const app = express();
 app.use(express.json());
 routes(app);
+
+app.use(manipuladorDeErros);
 
 export default app;
