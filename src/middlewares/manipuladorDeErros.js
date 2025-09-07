063629de4b8d59643e29d54df458ec2adb6b1418
mthquevedo <matheusq10@gmail.com>
2025-09-07 17:59:35 -0300

    Add: novo tratamento para erros de preenchimento obrigatório

--- src/middlewares/manipuladorDeErros.js
+++ src/middlewares/manipuladorDeErros.js
@@ -4,6 +4,12 @@ import mongoose from "mongoose";
 function manipuladorDeErros(erro, req, res, next) {
   if (erro instanceof mongoose.Error.CastError) {
     res.status(400).send({ message: "Um ou mais dados fornecidos estão incorretos" });
+  } else if (erro instanceof mongoose.Error.ValidationError) {
+    const messagensErro = Object.values(erro.errors)
+      .map(erro => erro.message)
+      .join("; ");
+
+    res.status(400).send({ message: `Os seguintes erros foram encontrados: ${messagensErro}` });
   } else {
     res.status(500).send({ message: "Erro interno de servidor." });
   };
