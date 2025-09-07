9cdb548c3d0ad0e017ea866d2794bbcc5ee926ed
mthquevedo <matheusq10@gmail.com>
2025-09-07 18:00:18 -0300

    Adjust: novas mensagens de erro para o schema

--- src/models/Autor.js
+++ src/models/Autor.js
@@ -2,9 +2,12 @@ import mongoose from "mongoose";
 
 const autorSchema = new mongoose.Schema(
   {
-    id: {type: String},
-    nome: {type: String, required: true},
-    nacionalidade: {type: String}
+    id: { type: String },
+    nome: {
+      type: String,
+      required: [true, "O nomde do(a) autor(a) é obrigatório"]
+    },
+    nacionalidade: { type: String }
   },
   {
     versionKey: false
