b14f5237e2792160f8e2cdca152d40df43fdd22f
mthquevedo <matheusq10@gmail.com>
2025-09-10 21:41:22 -0300

    Create: middleware de manipulacao de erros

--- src/middlewares/manipuladorDeErros.js
+++ src/middlewares/manipuladorDeErros.js
@@ -1,17 +1,19 @@
 import mongoose from "mongoose";
+import ErroBase from "../errors/ErroBase.js";
+import ErroValidacao from "../errors/ErroValidacao.js";
+import NaoEncontrado from "../errors/NaoEncontrado.js";
+import RequisicaoIncorreta from "../errors/RequisicaoIncorreta.js";
 
 // eslint-disable-next-line no-unused-vars
 function manipuladorDeErros(erro, req, res, next) {
   if (erro instanceof mongoose.Error.CastError) {
-    res.status(400).send({ message: "Um ou mais dados fornecidos estão incorretos" });
+    new RequisicaoIncorreta().enviarResposta(res);
   } else if (erro instanceof mongoose.Error.ValidationError) {
-    const messagensErro = Object.values(erro.errors)
-      .map(erro => erro.message)
-      .join("; ");
-
-    res.status(400).send({ message: `Os seguintes erros foram encontrados: ${messagensErro}` });
+    new ErroValidacao(erro).enviarResposta(res);
+  } else if (erro instanceof NaoEncontrado) {
+    erro.enviarResposta(res);
   } else {
-    res.status(500).send({ message: "Erro interno de servidor." });
+    new ErroBase().enviarResposta(res);
   };
 }
 
