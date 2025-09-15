a17ee892c5cefb26437954db842376fd92dc62f1
mthquevedo <matheusq10@gmail.com>
2025-09-15 20:48:35 -0300

    Add: validacoes para numeros de paginas e editora adicionadas

--- src/models/Livro.js
+++ src/models/Livro.js
@@ -14,9 +14,21 @@ const livroSchema = new mongoose.Schema(
     },
     editora: {
       type: String,
-      required: [true, "A editora é obrigatória"]
+      required: [true, "A editora é obrigatória"],
+      enum: {
+        values: ["Casa do Código", "Alura"],
+        message: "A editora ' {VALUE}' não é um valor permitido."
+      }
     },
-    numeroPaginas: { type: Number }
+    numeroPaginas: {
+      type: Number,
+      validate: {
+        validator: (valor) => {
+          return valor >= 10 && valor <= 5000;
+        },
+        message: "O número de páginas deve estar entre 10 e 5000. Valor fornecido: {VALUE}"
+      }
+    }
   }
 );
 
