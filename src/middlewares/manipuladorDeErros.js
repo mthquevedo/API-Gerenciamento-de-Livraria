7d927647727fe80170c1edb265eab1c3c708ce6a
mthquevedo <matheusq10@gmail.com>
2025-09-19 20:16:50 -0300

    Adjust: instancia de erro atualizada

--- src/middlewares/manipuladorDeErros.js
+++ src/middlewares/manipuladorDeErros.js
@@ -1,7 +1,6 @@
 import mongoose from "mongoose";
 import ErroBase from "../errors/ErroBase.js";
 import ErroValidacao from "../errors/ErroValidacao.js";
-import NaoEncontrado from "../errors/NaoEncontrado.js";
 import RequisicaoIncorreta from "../errors/RequisicaoIncorreta.js";
 
 // eslint-disable-next-line no-unused-vars
@@ -10,7 +9,7 @@ function manipuladorDeErros(erro, req, res, next) {
     new RequisicaoIncorreta().enviarResposta(res);
   } else if (erro instanceof mongoose.Error.ValidationError) {
     new ErroValidacao(erro).enviarResposta(res);
-  } else if (erro instanceof NaoEncontrado) {
+  } else if (erro instanceof ErroBase) {
     erro.enviarResposta(res);
   } else {
     new ErroBase().enviarResposta(res);
