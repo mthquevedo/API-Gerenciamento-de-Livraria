2c38c1eaf6cc05dcf0df50b2a5a5fc60acd715f9
mthquevedo <matheusq10@gmail.com>
2025-09-19 21:34:16 -0300

    Adjust: revisao de aviso de conexao

--- server.js
+++ server.js
@@ -4,5 +4,5 @@ import app from "./src/app.js";
 const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
-  console.log(`Servidor escutando em http://localhost:${port}`);
+  console.log(`✅ Servidor escutando em http://localhost:${port}`);
 });
--- src/middlewares/verficadorDeConexao.js
+++ src/middlewares/verficadorDeConexao.js
@@ -1,7 +1,7 @@
 function verificadorDeConexao(db) {
-  db.on("error", console.log.bind(console, "Erro de conexão"));
+  db.on("error", console.log.bind(console, "❌ Erro de conexão"));
   db.once("open", () => {
-    console.log("conexão com o banco feita com sucesso");
+    console.log("✅ Conexão com o banco feita com sucesso!");
   });
 }
 
