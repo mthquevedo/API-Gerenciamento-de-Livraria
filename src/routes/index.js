2bd3d8c5594eaf3e2b8a5599f261073d74c4227b
mthquevedo <matheusq10@gmail.com>
2025-09-19 21:35:05 -0300

    Adjust: titulo atualizado

--- src/routes/index.js
+++ src/routes/index.js
@@ -4,7 +4,7 @@ import autores from "./autoresRoutes.js";
 
 const routes = (app) => {
   app.route("/").get((req, res) => {
-    res.status(200).send({titulo: "Curso de node"});
+    res.status(200).send({titulo: "Sistema de gestão de livros"});
   });
 
   app.use(
